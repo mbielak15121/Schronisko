7e3c9f06685a0a6af034d95c34bf005802f05369
Szymon <szymon12110@wp.pl>
2022-06-05 14:59:57 +0200

    Dodanie ogłoszeń

---
+++ Schronisko/Models/Ogloszenie.cs
@@ -0,0 +1,12 @@
+﻿namespace Schronisko.Models
+{
+    public class Ogloszenie
+    {
+        public int Id { get; set; }
+        public string Imie { get; set; }
+        public string Rasa { get; set; }
+        public int Wiek { get; set; }
+        public string Opis { get; set; }
+
+    }
+}
