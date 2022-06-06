75f0633a51d2341ee1c962a547b7e4aae6e350db
Szymon Wojewódzki <81187@student.pb.edu.pl>
2022-06-06 13:27:59 +0200

    Wyszukiwanie po rasie lub wieku

--- Schronisko/Pages/PrzegladOgloszen/Index.cshtml.cs
+++ Schronisko/Pages/PrzegladOgloszen/Index.cshtml.cs
@@ -21,15 +21,23 @@ namespace Schronisko.Pages.PrzegladOgloszen
         }
 
         public IList<Ogloszenie> Ogloszenie { get;set; } = default!;
-
-        public async Task OnGetAsync(string searchString)
+        
+        public async Task OnGetAsync(string Opcja,string searchString)
         {
             var ogloszenia = from m in _context.Ogloszenie select m;
             
             if (!String.IsNullOrEmpty(searchString))
             {
+                if (Opcja == "Rasa")
+                {
+                    ogloszenia = ogloszenia.Where(s => s.Rasa.Contains(searchString));
+                }
+                else
+                {
+                    
+                    ogloszenia = ogloszenia.Where(s => s.Wiek.ToString().Contains(searchString));
+                }   
                 
-                ogloszenia = ogloszenia.Where(s => s.Rasa.Contains(searchString));
                 ogloszenia = ogloszenia.OrderByDescending(s => s.Id);
             }
           Ogloszenie=await ogloszenia.ToListAsync();
