fff190f834118c2ebc6221c285704b50a937c46b
kamilek <kamil.niczyporuk9@o2.pl>
2022-06-10 15:19:53 +0200

    poprawki

---
+++ Schronisko/Models/Harmonogram.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Schronisko.Models
+{
+    public class Harmonogram
+    {
+        public int Id { get; set; }
+        [Required]
+        public int Dni { get; set; }
+        [Required]
+        public string Godziny { get; set; }
+        [Required]
+        public string ZakresObowiazkow { get; set; }
+        
+    }
+}
